9e9eacbcaaa82d860aba074c2684239039fd0157
lanou3g <bluebird.qbbq@163.com>
2015-06-26 17:02:19 +0800

    TabBar 设置 ：TabBar buttons 标题(附上图片后才会显示) “首页”“活动”“拍摄”“频道”“我的”

--- TinyLipsAlphaIII/TinyLipsAlphaIII/APoject/Common/OursSDK/C/MineTabBarViewController.m
+++ TinyLipsAlphaIII/TinyLipsAlphaIII/APoject/Common/OursSDK/C/MineTabBarViewController.m
@@ -9,7 +9,7 @@
 #import "MineTabBarViewController.h"
 #import "FirstPageViewController.h"
 #import "ActivityPageViewController.h"
-
+#import "TakeShotPageViewController.h"
 #import "ChannelPageViewController.h"
 #import "UserPageViewController.h"
 
@@ -22,7 +22,62 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    // 配置viewControllers
+    [self configViewControllers];
 }
+
+- (void)configViewControllers
+{
+#pragma mark - FirstPageViewController
+    FirstPageViewController *firstVC = [[FirstPageViewController alloc]init];
+    // Set NavigationBar
+    UINavigationController *naFirstC = [[UINavigationController alloc]initWithRootViewController:firstVC];
+    firstVC.tabBarItem.title = @"首页";
+    firstVC.tabBarItem.image = [UIImage imageNamed:@"cinema"];
+    
+#pragma mark - ActivityPageViewController
+    ActivityPageViewController *activityVC = [[ActivityPageViewController alloc]init];
+    // Set NavigationBar
+    UINavigationController *naActivityC = [[UINavigationController alloc]initWithRootViewController:activityVC];
+    activityVC.tabBarItem.title = @"活动";
+    activityVC.tabBarItem.image = nil;
+    
+#pragma mark - TakeShotPageViewController
+    TakeShotPageViewController *takeShotVC = [[TakeShotPageViewController alloc]init];
+    // Set NavigationBar
+    UINavigationController *naTakeShotC = [[UINavigationController alloc]initWithRootViewController:takeShotVC];
+    takeShotVC.tabBarItem.title = @"拍摄";
+    takeShotVC.tabBarItem.image = nil;
+
+#pragma mark - ChannelPageViewController
+    ChannelPageViewController *channelVC = [[ChannelPageViewController alloc]init];
+    // Set NavigationBar
+    UINavigationController *naChannelC = [[UINavigationController alloc]initWithRootViewController:channelVC];
+    channelVC.tabBarItem.title = @"频道";
+    channelVC.tabBarItem.image = nil;
+
+#pragma mark - UserPageViewController
+    UserPageViewController *userVC = [[UserPageViewController alloc]init];
+    // Set NavigationBar
+    UINavigationController *naUserC = [[UINavigationController alloc]initWithRootViewController:userVC];
+    userVC.tabBarItem.title = @"我的";
+    userVC.tabBarItem.image = nil;
+
+    
+#pragma mark - 对视图控制器进行管理
+    self.viewControllers = @[naFirstC,naActivityC,naTakeShotC,naChannelC,naUserC];
+    
+}
+
+
+
+
+
+
+
+
+
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
