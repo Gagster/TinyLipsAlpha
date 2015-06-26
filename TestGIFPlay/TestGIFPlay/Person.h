5338ed28b377eb39bce7b40cf454a8cc95dee873
lanou3g <hewenhu264@126.com>
2015-06-26 10:49:04 +0800

    增加一个类 .....

---
+++ TestGIFPlay/TestGIFPlay/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  TestGIFPlay
+//
+//  Created by lanou3g on 15/6/26.
+//  Copyright (c) 2015年 Qi Junbao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
